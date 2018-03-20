b501109809825ba262a60edb9f63c00aba27864d
Ali-ozturk <aaztar17@student.aau.dk>
2018-03-20 12:33:26 +0100

    + Nodes now receives a Node-Type and their rightful names.

--- src/botenAnna/Node.java
+++ src/botenAnna/Node.java
@@ -7,11 +7,16 @@ public class Node {
     public ArrayList<Node> children;
     private String nodeName;
     private String lineOrigin;
+    private NodeTypes nodeType;
 
     public Node(String lineOrigin) {
         children = new ArrayList<Node>();
         this.lineOrigin = lineOrigin;
-        setName();
+        setNameAndType();
+    }
+
+    public enum NodeTypes {
+        SELECTOR, SEQUENCER, INVERTER, GUARD, TASK
     }
 
     public String getNodeName() {
@@ -26,8 +31,33 @@ public class Node {
         return children;
     }
 
-    private void setName(){
-        this.nodeName = getLineOrigin();
+    private void setNameAndType(){
+        String nodeName = getLineOrigin().trim().split(" ")[0];
+        this.nodeType = getNodeTypeFromString(nodeName);
+        this.nodeName = nodeName;
+    }
+
+    public NodeTypes getNodeType(){
+        return this.nodeType;
+    }
+
+    private NodeTypes getNodeTypeFromString(String nodeName){
+        switch(nodeName) {
+            case "Sequencer":
+                return NodeTypes.SEQUENCER;
+            case "Selector":
+                return NodeTypes.SELECTOR;
+            case "Inverter":
+                return NodeTypes.INVERTER;
+            default:
+               if (nodeName.length() >= 5 && nodeName.substring(0, 5).equals("Guard")) {
+                   return NodeTypes.GUARD;
+               } else if (nodeName.length() >= 4 && nodeName.substring(0, 4).equals("Task")){
+                   return NodeTypes.TASK;
+               }
+        }
+
+        return null;
     }
 
     public void addChild(Node node){
