4d03306601dacb7406870681a34d6a427c96d84b
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-03-26 12:08:15 +0200

    Added antialiasing to all graphics.

--- src/botenAnna/StructurePanel.java
+++ src/botenAnna/StructurePanel.java
@@ -45,6 +45,9 @@ public class StructurePanel extends JPanel {
 
     public void paint(Graphics g){
 
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
         // Find and add all Nodes which are going to be drawn
         ArrayList<Node> allNodes = new ArrayList<>();
         allNodes.add(mainNodeStructure); // Adding itself to the array first
@@ -57,19 +60,19 @@ public class StructurePanel extends JPanel {
 
         // Draw all nodes
         for(int i = 0; i < allNodes.size(); i++){
-            drawElement(g, allNodes.get(i));
+            drawElement(g2d, allNodes.get(i));
         }
 
         // Draw lines between nodes
         for(int i = 0; i < allNodes.size(); i++){
-            drawLines(g, allNodes.get(i));
+            drawLines(g2d, allNodes.get(i));
         }
     }
 
     /** This method draws a node.
-     * @param g a graphical element.
+     * @param g2d a graphical element.
      * @param node the node to be drawn. */
-    private void drawElement(Graphics g, Node node){
+    private void drawElement(Graphics2D g2d, Node node){
         int x = node.getXCoordinate();
         int y = node.getYCoordinate();
         int width = node.getGraphicalWidth();
@@ -78,37 +81,35 @@ public class StructurePanel extends JPanel {
         String txt = node.getNodeName();
         Image image = getTypeImage(type);
 
-        Graphics2D g2d = (Graphics2D) g;
-
         int borderThickness = 2;
         int edgeArc = 10;
         int imageSizeX = height, imageSizeY = height;
 
         //  | IMAGE | text       | //TODO used variable calculating from one to the next
         //Background box
-        g.setColor(Color.BLACK);
-        g.fillRoundRect(x, y, width, height, edgeArc, edgeArc);
+        g2d.setColor(Color.BLACK);
+        g2d.fillRoundRect(x, y, width, height, edgeArc, edgeArc);
 
         //Image and image background
         int imageWidth = imageSizeX - (2 * borderThickness);
         int imageHeight = imageSizeY - (2 * borderThickness);
-        g.setColor(getTypeColor(type));
-        g.fillRoundRect(x + borderThickness, y + borderThickness, imageWidth, imageHeight, edgeArc, edgeArc);
-        ((Graphics2D) g).drawImage(image, x, y, imageWidth, imageHeight, null); //Image
+        g2d.setColor(getTypeColor(type));
+        g2d.fillRoundRect(x + borderThickness, y + borderThickness, imageWidth, imageHeight, edgeArc, edgeArc);
+        g2d.drawImage(image, x, y, imageWidth, imageHeight, null); //Image
 
         //Text background and text
-        g.setColor(Color.WHITE);
-        g.fillRoundRect(x + imageWidth + (2 * borderThickness), y + borderThickness, width - imageWidth - (3 * borderThickness), height - ( 2 * borderThickness), edgeArc, edgeArc);
+        g2d.setColor(Color.WHITE);
+        g2d.fillRoundRect(x + imageWidth + (2 * borderThickness), y + borderThickness, width - imageWidth - (3 * borderThickness), height - ( 2 * borderThickness), edgeArc, edgeArc);
         //g.fillRoundRect(x + (imageSizeX + (1 * borderThickness)), y + borderThickness, width - imageSizeX - (2 * borderThickness), height - (3 * borderThickness), edgeArc, edgeArc);
-        g.setColor(Color.black);
-        drawText(g, node);
+        g2d.setColor(Color.black);
+        drawText(g2d, node);
 
     }
 
     /** Draws the text for a node
-     * @param g a graphical element.
+     * @param g2d a graphical element.
      * @param node the node with the text. */
-    private void drawText(Graphics g, Node node){
+    private void drawText(Graphics g2d, Node node){
         String txt = node.getNodeName();
         int imageSize = node.getGraphicalHeight();
         int x = node.getXCoordinate() + imageSize + 10;
@@ -116,7 +117,7 @@ public class StructurePanel extends JPanel {
 
         //Can the string be on one line
         if(txt.length() < 13)
-            g.drawString(txt, x, y);
+            g2d.drawString(txt, x, y);
         else{
 
             //Find the last upperCase / start of a word
@@ -136,20 +137,21 @@ public class StructurePanel extends JPanel {
                 secondLine = secondLine + txt.charAt(j);
             }
 
-            g.drawString(firstLine, x, y - 5);
-            g.drawString(secondLine, x, y + 10);
+            g2d.drawString(firstLine, x, y - 5);
+            g2d.drawString(secondLine, x, y + 10);
         }
     }
 
     /** Draws a lines between a node and its children.
-     * @param g a graphical element.
+     * @param g2d a graphical element.
      * @param node the node to draw lines from. */
-    private void drawLines(Graphics g, Node node){
+    private void drawLines(Graphics2D g2d, Node node){
+
         ArrayList<int[]> array = node.getChildrenCoordinates();
         int[] lineStart = new int[]{node.getXCoordinate(), node.getYCoordinate()};
 
         for(int i = 0; i < array.size(); i++){
-            g.drawLine(lineStart[0] + nodeWidth/2, lineStart[1] + nodeHeight, array.get(i)[0] + nodeWidth/2, array.get(i)[1]);
+            g2d.drawLine(lineStart[0] + nodeWidth/2, lineStart[1] + nodeHeight, array.get(i)[0] + nodeWidth/2, array.get(i)[1]);
         }
     }
 
